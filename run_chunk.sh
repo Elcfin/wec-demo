5c86ac292db56e5978539439cd40e97000eeb581
Elcfin <1181535627@qq.com>
2025-06-26 12:15:00 +0800

    update sh

--- run_chunk.sh
+++ run_chunk.sh
@@ -3,11 +3,11 @@
 # Global parameters for the experiment
 k=128
 m=4
-b=16 # MB
-s=32
+b=32 # MB
+s=16
 f=/home/elcfin/shm
-xy=256
-chunk_size=128 # KB
+xy=128
+chunk_size=1024 # KB
 x_values=(4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42 44 46 48 50 52 54 56 58 60 62 64)
 
 # Check if temporary directory exists, create it if not
